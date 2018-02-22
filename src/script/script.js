b980a7ea13faaaa1a9c356af0aafaf320ecd6460
Yurii <gawrelyuk@gmail.com>
2018-02-23 00:08:24 +0200

    added a responsive style

--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,36 @@
 
-//tabs
-(function ($, window, document) {
-    $(function () {
-        tab.init();
-      });
 
-    $(function() {
-      $('.js-tab-titles').on('click', '.tab__title:not(.active)', function() {
-        $(this).addClass('active').siblings().removeClass('active')
-          .closest('.tab').find('.js-tab-item').removeClass('active').eq($(this).index()).addClass('active');
-      });
+(function ($, window, document) {
+
+    //Implementation of tabs
+    $(document).ready(function() {
+        $('.js-tab').easyResponsiveTabs({
+            type: 'vertical',
+            tabidentify: 'hor_1',
+        });
+    });
+
+//Implementation of animation for collasible tabs (desctop)
+    $('.tab__title').click(function() {
+        if(!$(this).hasClass('resp-tab-active')){
+            $('.resp-tab-content').animate({
+                opacity: 0,   
+              }, 500);  
+              $('.resp-tab-content').animate({
+                opacity: 1,         
+              }, 500); 
+        }
     
     });
+
+//Implementation of scrolling
+    $(".js-scroll").on("click", function (e) {
+      e.preventDefault();
+      var id  = $(this).attr('href'),   
+          top = $(id).offset().top;
+    
+      $('body,html').animate({scrollTop: top}, 1000);
+    });
+  
 }(window.jQuery, window, document));
+
